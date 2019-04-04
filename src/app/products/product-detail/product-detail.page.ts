d37c9f1171eb44ae6cb5dd63c45b9c5ba9bb394b
Justyna Rodzevic <justyna.rodzevic@gmail.com>
2019-04-04 20:47:43 +0100

    More work done on product details page. Navigation added

--- src/app/products/product-detail/product-detail.page.ts
+++ src/app/products/product-detail/product-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../products.service';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.page.scss'],
 })
 export class ProductDetailPage implements OnInit {
+  loadedProduct: Product;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private productsService: ProductsService
+    ) { }
 
   ngOnInit() {
+    // map all parameter it receives
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+
+      // check if parameter does not have productId
+      if (!paramMap.has('productId')) {
+
+        // redirect user
+      }
+      // extract productId
+      const productId = paramMap.get('productId');
+
+      //use service to load product by id
+      this.loadedProduct = this.productsService.getProduct(productId);
+    });
   }
 
 }

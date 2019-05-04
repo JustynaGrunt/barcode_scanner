49c2f54ce78cbae2bbab1f1427d2dedb0054d7cc
Justyna Rodzevic <justyna.rodzevic@gmail.com>
2019-05-04 12:36:51 +0100

    products search function

--- src/app/products/products.page.ts
+++ src/app/products/products.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from './product.model';
-import { ProductsService } from './products.service';
+// import { ProductsService } from './products.service';
+import { Observable } from 'rxjs';
+import { SearchType, ProductService } from '../services/product.service';
+
 
 @Component({
   selector: 'app-products',
@@ -8,12 +11,32 @@ import { ProductsService } from './products.service';
   styleUrls: ['./products.page.scss'],
 })
 export class ProductsPage implements OnInit {
-  products: Product [];
+  products: Observable<any>;
+  searchTerm = '';
+  type: SearchType = SearchType.all;
 
-  constructor(private productsService: ProductsService) { }
+ // products: Product [];
+  constructor(private productsService: ProductService) { }
 
   ngOnInit() {
-    this.products = this.productsService.getAllProducts();
+    //this.products = this.productsService.getAllProducts();
+    //this.getProducts();
   }
+
+  searchChanged(){
+    //call searchData service
+    this.products = this.productsService.searchData(this.searchTerm, this.type);
+  }
+
+  // getProducts(){
+  //   this.productsService.getAllProducts()
+  //   .subscribe(res => {
+  //     this.products = res;
+  //     console.log( this.products);
+  //   }, error => {
+  //     console.log(error);
+  //   });
+  // }
+
 
 }
